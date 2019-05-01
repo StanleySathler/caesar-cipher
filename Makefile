22218a63da73df04a6f3067dce9eea5893c5d7e1
Stanley Sathler <stanleysathlerpinto@gmail.com>
2019-05-01 19:14:40 -0300

    feat: add "key" param to the usage command

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ compile: src/caesar_cipher.o src/main.o
 	gcc -o bin/main.bin src/caesar_cipher.o src/main.o
 
 exec_e:
-	./bin/main.bin -e Stanley
+	./bin/main.bin -e 5 Stanley
 
 exec_d:
-	./bin/main.bin -d vwdqohb
+	./bin/main.bin -d 5 xyfsqjd
--- src/main.c
+++ src/main.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "caesar_cipher.h"
 
 #define IDX_MODE_PARAM  1
-#define IDX_TEXT        2
+#define IDX_KEY         2
+#define IDX_TEXT        3
 
 int
 main(int argc, char **argv)
 {
   int is_encoding = !(strcmp(argv[IDX_MODE_PARAM], "-e"));
   int is_decoding = !(strcmp(argv[IDX_MODE_PARAM], "-d"));
+  int key = atoi(argv[IDX_KEY]);
 
-  if (argc < 3 || (!is_encoding && !is_decoding))
+  if (argc < 4 || (!is_encoding && !is_decoding))
   {
-    fprintf(stderr, "[ERR]: You must specify a mode and a text. Usage: %s [-e | -d] TEXT \n", argv[0]);
+    fprintf(stderr, "[ERR]: You must specify a mode and a text. Usage: %s [-e | -d] <key> <text> \n", argv[0]);
     return(-1);
   }
 
   if (is_encoding)
-    printf("%s", caesar_cipher_encode(argv[IDX_TEXT], 3));
+    printf("%s", caesar_cipher_encode(argv[IDX_TEXT], key));
   else if (is_decoding)
-    printf("%s", caesar_cipher_decode(argv[IDX_TEXT], 3));
+    printf("%s", caesar_cipher_decode(argv[IDX_TEXT], key));
 
   putchar('\n');
   return(0);
